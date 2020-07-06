5e9c41f820725d18d56832716a558ef9943f939c
hosuchoi <hosuchoi>
2020-07-07 00:25:34 +0900

    WebMvcConfigurer

--- src/main/java/lake/pool/WebApplication.java
+++ src/main/java/lake/pool/WebApplication.java
@@ -9,10 +9,12 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 //spring 3.1 , servlet 3.0 부터 지원
+//web.xml 기능을 자바class로 처리 할수 있음
 public class WebApplication implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setServletContext(servletContext); //@EnableWebMvc 사용하려면 등록해야됨 이유는 @WebMVC에서 해당 servletContext를 사용함
         context.register(WebConfig.class);
         context.refresh();
 
--- src/main/java/lake/pool/WebConfig.java
+++ src/main/java/lake/pool/WebConfig.java
@@ -1,13 +1,65 @@
 package lake.pool;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /*
 DispatcherServlet에서 생성한 applicationContext
  */
 @Configuration
 @ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class)) // 모든 bean 등록 false 후 Controller만 빈으로 등록 하겠다
-public class WebConfig {
+@EnableWebMvc // 아래 빈을 등록하여 처리하는 rowlevel 방식보다 발전된 방식
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/",".jsp");
+    }
+
+    /*
+    implements WebMvcConfigurer 사용시 아래 빈설정을 편하게 해줌 ( 아래 다 주석 처리 )
+     */
+
+    /*
+
+//    아래의 빈 등록후 처리하는 방식은
+//    가장 row level의 spring mvc 설정 방식
+
+    @Bean
+    public HandlerMapping handlerMapping(){
+        RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
+        handlerMapping.setInterceptors();
+        handlerMapping.setOrder((Ordered.HIGHEST_PRECEDENCE));
+        return handlerMapping;
+    }
+
+    @Bean
+    public HandlerAdapter handlerAdapter(){
+        RequestMappingHandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
+        return handlerAdapter;
+    }
+
+//    viewResolver를 빈등록 하여서 prefix, suffix 설정함 ( controller에서 view 경로에 생략가능 )
+//    그외에 viewResoler를 통해서 정의 가능
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+*/
 }
